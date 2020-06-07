8e420c82c56b0ff1f9ddf5f99e1bea70ea205b4a
Sabrina Li <sabrinali0312@gmail.com>
2020-06-07 12:10:22 -0400

    initial boilerplate files

---
+++ Example/FullStorySegmentMiddleware/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FullStorySegmentMiddleware
+//
+//  Created by FullStory on 06/07/2020.
+//  Copyright (c) 2020 FullStory. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ViewController : UIViewController
+
+@end
