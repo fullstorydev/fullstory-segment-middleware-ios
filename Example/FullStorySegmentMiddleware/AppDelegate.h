1a084a5456134f7e8df135963fc52c9cf1056cc9
sabrina-li <sabrinali0312@gmail.com>
2020-06-10 17:29:00 -0400

    initial boilerplate files (#2)

---
+++ Example/FullStorySegmentMiddleware/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MYAppDelegate.h
+//  FullStorySegmentMiddleware
+//
+//  Created by FullStory on 06/07/2020.
+//  Copyright (c) 2020 FullStory. All rights reserved.
+//
+
+@import UIKit;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/FullStorySegmentMiddleware/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FullStorySegmentMiddleware
+//
+//  Created by FullStory on 06/07/2020.
+//  Copyright (c) 2020 FullStory. All rights reserved.
+//
+
+@import UIKit;
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
