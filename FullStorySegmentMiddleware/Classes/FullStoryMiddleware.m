8fd7670cb79b88db00e97a5f67d9973b29562779
Sabrina Li <sabrinali0312@gmail.com>
2020-06-15 11:41:26 -0400

    add comments in fullstory middleware

--- FullStorySegmentMiddleware/Classes/FullStoryMiddleware.m
+++ FullStorySegmentMiddleware/Classes/FullStoryMiddleware.m
@@ -25,16 +25,17 @@
 }
 
 - (id)init{
-    //init with no whitelisted events
+    // Init with no whitelisted events
     return [self initWithWhitelistEvents:nil];
 }
 
 - (void)context:(SEGContext * _Nonnull)context next:(SEGMiddlewareNext _Nonnull)next {
     next([context modify:^(id<SEGMutableContext>  _Nonnull ctx) {
-        //TODO: add support for Options
+        // TODO: add support for Options
         switch(ctx.eventType){
             case SEGEventTypeGroup:{
                 SEGGroupPayload *payload = (SEGGroupPayload *) ctx.payload;
+                // Create mutable userVars and optionally add the group traits as userVars to be assoicated to the user in FullStory
                 NSMutableDictionary *userVars = [[NSMutableDictionary alloc] initWithObjectsAndKeys:payload.groupId,@"groupID", nil];
                 if(self.enableGroupTraitsAsUserVars){
                     [userVars addEntriesFromDictionary:payload.traits];
@@ -44,11 +45,14 @@
             }
             case SEGEventTypeIdentify:{
                 SEGIdentifyPayload *payload = (SEGIdentifyPayload *) ctx.payload;
+                // Segment Identify event, identify the same userID and traits in FullStory
+                // if displayName is not set, by default, user email is the displayName
                 [FS identify: payload.userId userVars:payload.traits];
                 break;
             }
             case SEGEventTypeScreen:{
                 SEGScreenPayload *payload = (SEGScreenPayload *) ctx.payload;
+                // Segment Screen event, optionally enabled and send as custom events into FullStory
                 if(self.enableSendScreenAsEvents){
                     NSString *name = [[NSString alloc] initWithFormat:@"Segment Screen: %@",payload.name];
                     [FS event:name properties:payload.properties];
@@ -57,20 +61,29 @@
             }
             case SEGEventTypeTrack: {
                 SEGTrackPayload *payload = (SEGTrackPayload *) ctx.payload;
+                // Segment Track event, optionally enabled /w events whitelisted, send as custom events into FullStory
                 if(self.whitelistAllTrackEvents || [self.whitelistEvents containsObject:payload.event]){
                     [FS event:payload.event properties:payload.properties];
                 }
                 break;
             }
             case SEGEventTypeReset:{
+                // Segment Reset, usually called when user logs out, so anonymize the user in FullStory
                 [FS anonymize];
                 break;
             }
             default:{}
         }
+        
+        // Always log the segment event with INFO level to FullStory dev tools
         [FS logWithLevel:FSLOG_INFO format:@"Segment event type: %@", [self getEventName:ctx.eventType]];
         
+        // Create local var: payload, is not nil only when the evnet is Track or Screen
         SEGPayload *payload = [self getNewPayloadWithFSURL:context];
+        
+        // Only override the ctx.payload if:
+        // - is Track or Screen event
+        // - enabled FS session URL as part of the track/screen event properties
         if(self.enableFSSessionURLInEvents && payload != nil){
             ctx.payload = payload;
         }
@@ -78,30 +91,38 @@
 }
 
 - (SEGPayload *) getNewPayloadWithFSURL:(SEGContext * _Nonnull)context {
-    if(context.eventType == SEGEventTypeTrack){
-        SEGTrackPayload *trackPayload = (SEGTrackPayload *) context.payload;
-        NSMutableDictionary *newProps = [[NSMutableDictionary alloc] initWithDictionary:trackPayload.properties];
-        [newProps setValue:[FS currentSessionURL] forKey:@"FSSessionURL"];
-        
-        SEGTrackPayload *newPayload = [[SEGTrackPayload alloc]
-                                       initWithEvent:trackPayload.event
-                                       properties:newProps
-                                       context:trackPayload.context
-                                       integrations:trackPayload.integrations];
-        return newPayload;
-    }else if(context.eventType == SEGEventTypeScreen){
-        SEGScreenPayload *screenPayload = (SEGScreenPayload *) context.payload;
-        NSMutableDictionary *newProps = [[NSMutableDictionary alloc] initWithDictionary:screenPayload.properties];
-        [newProps setValue:[FS currentSessionURL] forKey:@"FSSessionURL"];
-        
-        SEGScreenPayload *newPayload = [[SEGScreenPayload alloc]
-                                       initWithName:screenPayload.name
-                                       properties:screenPayload.properties
-                                       context:screenPayload.context
-                                       integrations:screenPayload.integrations];
-        return newPayload;
+    // Return not nil when we find Track and Screen event, and insert FS session URL into event properties
+    SEGPayload *newPayload = nil;
+    switch (context.eventType) {
+        case SEGEventTypeTrack:{
+            SEGTrackPayload *trackPayload = (SEGTrackPayload *) context.payload;
+            NSMutableDictionary *newProps = [[NSMutableDictionary alloc] initWithDictionary:trackPayload.properties];
+            [newProps setValue:[FS currentSessionURL] forKey:@"FSSessionURL"];
+            
+            newPayload = [[SEGTrackPayload alloc]
+                                           initWithEvent:trackPayload.event
+                                           properties:newProps
+                                           context:trackPayload.context
+                                           integrations:trackPayload.integrations];
+            break;
+        }
+        case SEGEventTypeScreen:{
+            SEGScreenPayload *screenPayload = (SEGScreenPayload *) context.payload;
+            NSMutableDictionary *newProps = [[NSMutableDictionary alloc] initWithDictionary:screenPayload.properties];
+            [newProps setValue:[FS currentSessionURL] forKey:@"FSSessionURL"];
+            
+            newPayload = [[SEGScreenPayload alloc]
+                                           initWithName:screenPayload.name
+                                           properties:screenPayload.properties
+                                           context:screenPayload.context
+                                           integrations:screenPayload.integrations];
+            break;
+        }
+        default:
+            newPayload = nil;
+            break;
     }
-    return nil;
+    return newPayload;
 }
 
 // get all possible events from Event integer enum: https://segment.com/docs/connections/sources/catalog/libraries/mobile/ios/#usage
