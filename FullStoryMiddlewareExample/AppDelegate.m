76b408d1389c2db8c7d38677535e4bc891a0e840
Sabrina Li <sabrinali0312@gmail.com>
2020-11-16 14:22:09 -0500

    add FSM and Segment Analytics, and buttons to trigger Segment events

--- FullStoryMiddlewareExample/AppDelegate.m
+++ FullStoryMiddlewareExample/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import <Analytics/SEGAnalytics.h>
+#import <FullStoryMiddleware/FullStoryMiddleware.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +18,35 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    // set certain events to be the only ones allowed to forward to FullStory
+    NSArray *allowList = @[@"Product Viewed", @"Order Completed", @"Completed Checkout Step"];
+    FullStoryMiddleware *fsm = [[FullStoryMiddleware alloc] initWithAllowlistEvents: allowList];
+    
+    // enable to insert FS session URL to Segment event properties and contexts
+    // default to true
+    fsm.enableFSSessionURLInEvents = true;
+    // when calling Segment group, send group traits as userVars
+    // default to false
+    fsm.enableGroupTraitsAsUserVars = true;
+    // when calling Segment screen, sent the screen event as custom events to FS
+    // default to false
+    fsm.enableSendScreenAsEvents = true;
+    // allow all track events as FS custom events
+    // alternatively allow list events that you would like to track
+    // note: enabling this will cause the middleware to ignore the event allowlist
+    // default to false
+    fsm.allowlistAllTrackEvents = true;
+    
+    
+    SEGAnalyticsConfiguration *configuration = [SEGAnalyticsConfiguration configurationWithWriteKey:@"jg953hydkaC9F8ViwqfMwfufFW9FlwJT"];
+    configuration.trackApplicationLifecycleEvents = YES;
+    configuration.recordScreenViews = YES;
+    
+    // Add FullStoryMiddleware as one of the sourceMiddlewares
+    configuration.middlewares = @[fsm];
+    [SEGAnalytics setupWithConfiguration:configuration];
+
     return YES;
 }
 
--- Podfile
+++ Podfile
@@ -1,8 +1,8 @@
-platform :ios, '9.0'
+platform :ios, '10.0'
 
 def common_pods
-  pod 'FullStory', :http => 'https://ios-releases.fullstory.com/fullstory-1.7.0.tar.gz'
-  pod 'Analytics', '~> 3.0'
+  pod 'FullStory', :http => 'https://ios-releases.fullstory.com/fullstory-1.9.0.tar.gz'
+  pod 'Analytics', '~> 4.1'
 end
 
 target 'FullStoryMiddlewareExample' do
